671baed94d1670dded0649fe15aff2b082baf8f9
Angelo Almeida <angelo94.aa@gmail.com>
2022-09-26 16:51:07 -0500

    actualizando carpeta javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal()
+
+sr.reveal('#article', {
+	duration: 3000,
+	origin: 'bottom',
+	distance: '100px',
+})
+
+sr.reveal('#section', {
+	duration: 3000,
+	origin: 'left',
+	distance: '100px',
+})
+sr.reveal('#footer', {
+	duration: 3000,
+	origin: 'bottom',
+	distance: '100px',
+})
